ea0ebf4ed3fc18ee46aac2d84a51fb97c7efcf9a
Ghizlane Ghaya <gghaya@e2r8p7.1337.ma>
2025-01-13 02:08:08 +0100

    done hamdullah

--- test/setup.sh
+++ test/setup.sh
@@ -5,8 +5,8 @@ bin/elasticsearch-certutil cert --silent --pem -out config/certs/certs.zip --in 
 unzip config/certs/certs.zip -d config/certs;
 # export ELASTIC_PASSWORD=${ELASTIC_PASSWORD}
 # sleep 30
-exec /usr/share/elasticsearch/bin/elasticsearch
-# sleep 100
+# exec /usr/share/elasticsearch/bin/elasticsearch
+until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://elasticsearch:9200/_security/user/kibana_system/_password -d "{\"password\":\"${KIBANA_PASSWORD}\"}" | grep -q "^{}"; do sleep 15; done;
 # export ELASTIC_PASSWORD=$(echo "y" | bin/elasticsearch-reset-password -u elastic | grep -oP '(?<=New value: ).*')
 # echo "New password for the elastic user: $ELASTIC_PASSWORD"
 # curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://127.0.0.1:9200/_security/user/kibana_system/_password -d "{\"password\":\"${KIBANA_PASSWORD}\"}"
